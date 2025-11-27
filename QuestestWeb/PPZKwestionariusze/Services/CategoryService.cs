ae7e54ae55c6e65c349b4344e9667fe5a68e176a
Gabisia <paluszek147@gmail.com>
2025-11-27 10:54:50 +0100

    Utworzenie serwisu do obsługi kategorii w aplikacji webowej

---
+++ QuestestWeb/PPZKwestionariusze/Services/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using PPZKwestionariusze.Models;
+
+namespace PPZKwestionariusze.Services
+{
+    public class CategoryService
+    {
+        private readonly string _connectionString;
+
+        public CategoryService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            var categories = new List<Category>();
+
+            using (var connection = new OracleConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                var command = new OracleCommand("SELECT ID, NAME FROM CATEGORIES", connection);
+
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    categories.Add(new Category
+                    {
+                        CategoryId = reader.GetInt32(0),
+                        CategoryName = reader.GetString(1)
+                    });
+                }
+            }
+
+            return categories;
+        }
+    }
+}
